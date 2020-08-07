792a86d2c23eb01f3a7d0f3414f6fd84e457527f
Keiji ARIYAMMA <keiji_ariyama@c-lis.co.jp>
2020-08-07 16:49:06 +0900

    create project

---
+++ app/src/main/java/jp/co/c_lis/bookviewer/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.c_lis.bookviewer.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
