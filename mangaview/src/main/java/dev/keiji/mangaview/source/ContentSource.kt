82bc0fb8f6df1c9f7068a76fa39d066e627e0851
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-02-02 01:08:48 +0900

    Documented

--- mangaview/src/main/java/dev/keiji/mangaview/source/ContentSource.kt
+++ mangaview/src/main/java/dev/keiji/mangaview/source/ContentSource.kt
@@ -4,18 +4,47 @@ import dev.keiji.mangaview.widget.ViewContext
 
 abstract class ContentSource {
 
+    /**
+     * The state of the content source.
+     */
     enum class State {
         NA,
         Preparing,
         Prepared,
     }
 
+    /**
+     * The width property of the content.
+     */
     abstract val contentWidth: Float
+
+    /**
+     * The height property of the content.
+     */
     abstract val contentHeight: Float
 
+    /**
+     * Gets the state of the content source.
+     *
+     * @param viewContext The ViewContext the MangaView is running in
+     *
+     * @return The state of the content source: one of `State.NA`, `State.Preparing` or `State.Prepared`.
+     */
     abstract fun getState(viewContext: ViewContext): State
 
+    /**
+     * Prepare the content source.
+     *
+     * @param viewContext
+     * @param onPrepared
+     *
+     * @return `true` the content is prepared,
+     *         `false` otherwise
+     */
     abstract fun prepare(viewContext: ViewContext, onPrepared: () -> Unit): Boolean
 
+    /**
+     * Free objects associated with this source.
+     */
     abstract fun recycle()
 }
